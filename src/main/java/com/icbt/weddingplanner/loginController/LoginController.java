963fba9276b02fb5c8ac87a141c532c189d018c7
Sandeepa <nuwansandeepa3@gmail.com>
2021-02-05 09:16:18 +0530

    remove request body annotation

--- src/main/java/com/icbt/weddingplanner/loginController/LoginController.java
+++ src/main/java/com/icbt/weddingplanner/loginController/LoginController.java
@@ -9,15 +9,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(path = "api/v1/login")
+@RequestMapping(path = "api/v1/login",consumes = { MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE })
 @AllArgsConstructor
 public class LoginController {
 
     private final AppUserService appUserService;
 
-    @PostMapping(produces = {MediaType.APPLICATION_JSON_VALUE,MediaType.
-            APPLICATION_XML_VALUE})
-    public LoginResponse login(@RequestBody LoginRequest loginRequest){
+    @PostMapping(consumes = MediaType.ALL_VALUE)
+    public LoginResponse login(LoginRequest loginRequest){
         return appUserService.login(loginRequest);
     }
 
